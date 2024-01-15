c885b13d497f7ea34976d3e24cc9a1c617124b1e
jjk <jinat1270@gmail.com>
2024-01-15 18:40:28 +0600

    fix: Add frontend validation for registration page.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,21 +9,21 @@
                 <form method="POST" action="{{ route('register') }}" class="bg-white shadow-md rounded-md p-10 w-96">
                     @csrf
                     <label class="text-sm mb-2">User Name</label>
-                    <input type= "text" name="user_name" value="{{ old('user_name') }}"
+                    <input type= "text" name="user_name" required value="{{ old('user_name') }}"
                         class="border px-3 py-2 mb-4 w-full @error('user_name') !border-red-500 @enderror">
                     @error('user_name')
                         <p class="text-red-600 text-sm">{{ $message }}</p>
                     @enderror
                     <br>
                     <label class="text-sm mb-2">Email</label>
-                    <input type= "email" name="email" value="{{ old('email') }}"
+                    <input type= "email" name="email" required value="{{ old('email') }}"
                         class="border px-3 py-2 mb-4 w-full @error('email') !border-red-500 @enderror">
                     @error('email')
                         <p class="text-red-600 text-sm">{{ $message }}</p>
                     @enderror
                     <br>
                     <label class="text-sm mb-2">Password</label>
-                    <input type= "password" name="password"
+                    <input type= "password" name="password" required
                         class="border px-3 py-2 mb-4 w-full @error('password') !border-red-500 @enderror">
                     @error('password')
                         <p class="text-red-600 text-sm">{{ $message }}</p>
